0fb097a2217cb057875e178a82b181c055e68f2c
michelherv <michel-herve.ngounou@wizbii.com>
2017-10-23 19:20:39 +0200

    adds routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CompanyService } from './services/company-service';
+import { ContractService } from './services/contract-service';
+import { CountryService } from './services/country-service';
+import { DomainService } from './services/domain-service';
+import { JobService } from './services/job-service';
+import { JobModule } from '../libs/job-lib/job.module';
+
+import { SearchJobComponent } from './views/jobs/search-job/search-job.component';
+import { ViewJobComponent } from './views/jobs/view-job/view-job.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchJobComponent,
+    ViewJobComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    JobModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+    CompanyService,
+    ContractService,
+    CountryService,
+    DomainService,
+    JobService,
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
